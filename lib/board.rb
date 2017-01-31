ade7233182d799d7a015d3e73dc5568636ee748a
Fernanda <fernandamora88@gmail.com>
2017-01-30 22:59:46 -0800

    add place_stones and make_move

--- lib/board.rb
+++ lib/board.rb
@@ -1,21 +1,56 @@
+require 'byebug'
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14){Array.new}
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_index do |index|
+      @cups[index] = Array.new(4){:stone} unless index == 6 || index == 13
+    end
   end
 
   def valid_move?(start_pos)
+    start_pos -= 1
+
+    unless in_bounds?(start_pos)
+      raise "Invalid starting cup"
+    end
+  end
+
+  def in_bounds?(position)
+    position >= 0 && position <= @cups.length - 1
   end
 
   def make_move(start_pos, current_player_name)
+    debugger
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+    next_pos = circular_position(start_pos + 1)
+    stones.each do |stone|
+      if next_pos == cup_from_position(start_pos)
+        next_pos = circular_position(next_pos + 1)
+      end
+      @cups[next_pos] << stone
+      next_pos = circular_position(next_pos + 1)
+    end
+    render
+    next_turn(circular_position(next_pos))
+  end
+
+  def cup_from_position(position)
+    position <= 6 ? 7 : 13
+  end
+
+  def circular_position(position)
+    position % 14
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine what #make_move returns
   end
 
   def render
